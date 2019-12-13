fd835230fea03a3e25ca0cee2480060465270441
matthias <mr3lucciola@gmail.com>
2019-12-13 15:00:42 -0500

    attempting to fix login issues on remote server

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -128,6 +128,8 @@ if "/www/" in BASE_DIR:
     from os import environ
     DATABASES["USER"] = environ.get("diffuse_user")
     DATABASES["PASSWORD"] = environ.get("diffuse_pw")
+    DATABASES["HOST"] = '127.0.0.1'
+    DATABASES["PORT"] = '27017'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
