7459e6aa9e1d881252ffffaf6209670a990e269b
matthias <mr3lucciola@gmail.com>
2019-12-13 17:35:47 -0500

    updated the init.py settings file to work in prod environment

--- backend/backend/settings/__init__.py
+++ backend/backend/settings/__init__.py
@@ -1,7 +1,6 @@
 from .base import *
 
-from .local import *
-#if "/var/www/" not in __file__:
-#    from .local import *
-#else:
-#    from .production import *
+if "/var/www/" not in __file__:
+   from .local import *
+else:
+   from .production import *
