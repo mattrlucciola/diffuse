bf92e428fc2871070d78b42b569a928568e56088
matthias <mr3lucciola@gmail.com>
2019-12-13 18:03:35 -0500

    modified settings files to appropriately set vars

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -169,16 +169,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 
+STATIC_ROOT = f"{os.path.dirname(BASE_DIR)}/client/build/"
 
-CORS_REPLACE_HTTPS_REFERER      = False
-HOST_SCHEME                     = "http://"
-SECURE_PROXY_SSL_HEADER         = None
-SECURE_SSL_REDIRECT             = False
-SESSION_COOKIE_SECURE           = False
-CSRF_COOKIE_SECURE              = False
-# CSRF_USE_SESSIONS               = True
-# CSRF_COOKIE_HTTPONLY            = False
-SECURE_HSTS_SECONDS             = None
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
-SECURE_FRAME_DENY               = False
+CSRF_USE_SESSIONS = True
