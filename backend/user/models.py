0a20683504f2327c286d5a6713cd625f274b05d7
matthias <mr3lucciola@gmail.com>
2019-12-13 06:08:04 -0500

    added createsuperuser method to the user class

--- backend/user/models.py
+++ backend/user/models.py
@@ -6,28 +6,45 @@ from django.contrib.auth.models import AbstractUser
 
 class UserManager(models.Manager):
     
-    def _create_user(self, username, email, password, **extra_fields):
+    def create_user(self, email, password=None,is_active=True,is_staff=False,is_admin=False):
         """
-        Create and save a user with the given username, email, and password.
+        Creates and saves a User with the given email and password.
         """
-        if not username:
-            raise ValueError('The given username must be set')
-        email = self.normalize_email(email)
-        username = self.model.normalize_username(username)
-        user = self.model(username=username, email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(username, email, password, **extra_fields)
+    def create_staffuser(self, email, password):
+        """
+        Creates and saves a staff user with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
     
     def get_by_natural_key(self, username):
         return self.get(username=username)
