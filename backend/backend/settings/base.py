4fc3bb19dbb45d373dead345541413feaa503566
matthias <mr3lucciola@gmail.com>
2019-12-13 20:08:08 -0500

    changed

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -15,6 +15,7 @@ from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+CLIENT_DIR = f"{os.path.dirname(BASE_DIR)}/client"
 # print(f"{os.path.dirname(BASE_DIR)}/client/")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'rest_auth',
     
     # my apps
+    'core',
     'user',
     'project',
     # 'comment',
@@ -98,7 +100,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [CLIENT_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -169,6 +171,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = f"{os.path.dirname(BASE_DIR)}/client/build/"
+# STATIC_ROOT = os.path.join(CLIENT_DIR,'build')# f"{os.path.dirname(BASE_DIR)}/client/build/"
+# print(BASE_DIR)
+# print(CLIENT_DIR)
+# print(STATIC_ROOT)
+STATICFILES_DIRS = (
+    os.path.join(CLIENT_DIR, "build", "static"),  # update the STATICFILES_DIRS
+)
 
 CSRF_USE_SESSIONS = True
