823f5956ccf47eab9d04c67f8ead97bf2ee049a0
matthias <mr3lucciola@gmail.com>
2020-01-05 12:34:11 -0500

    cleaned errors in slugify.js

--- client/src/util/slugify.js
+++ client/src/util/slugify.js
@@ -4,11 +4,11 @@ export const slugify = (string) => {
     const p = new RegExp(a.split('').join('|'), 'g')
   
     return string.toString().toLowerCase()
-        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(' ', '-') // Replace spaces with -
         .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-        .replace(/&/g, '-and-') // Replace & with 'and'
-        .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-        .replace(/\-\-+/g, '-') // Replace multiple - with single -
+        .replace('&', '-and-') // Replace & with 'and'
+        .replace(/\W/g, '') // Remove all non-word characters
+        .replace('----', '-').replace('---', '-').replace('--', '-') // Replace multiple - with single -
         .replace(/^-+/, '') // Trim - from start of text
         .replace(/-+$/, '') // Trim - from end of text
 }
