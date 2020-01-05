13ca50a3a0e3448123eabd9ef1b79f4396c61633
matthias <mr3lucciola@gmail.com>
2020-01-05 13:11:22 -0500

    added placeholder sidebar to sidebarleft

--- client/src/components/SidebarLeft.jsx
+++ client/src/components/SidebarLeft.jsx
@@ -4,13 +4,20 @@
 import React from 'react';
 
 // components
-import ProjectSidebar from './sidebar/ProjectSidebar';
+import ProjectSidebar from './project/ProjectSidebar';
 
 // start
 export default function SidebarLeft({loggedIn}){
+    const placeholderSidebar = () => {
+        return(
+            <div className="left-sidebar placeholderSidebar">
+                Click to sign in or sign up here!
+            </div>
+        )
+    }
     return(
         <div className="SidebarLeft">
-            {loggedIn && <ProjectSidebar />}
+            {loggedIn ? <ProjectSidebar />: placeholderSidebar()}
         </div>
     )
 }
